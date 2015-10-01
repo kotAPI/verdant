14cc206176a5b800f0a59811264d9f28d38fc354
Pranay Kothapalli <pranaykothapalli@gmail.com>
2015-10-02 02:14:19 +0530

    update video

--- script/main.js
+++ script/main.js
@@ -3,29 +3,40 @@ var playing=false;
 var looper=0;
 
 
+
+
 window.onload=function(){
 	Init();
+	
 }
 function Init()
 {
 	for (looper=0;looper<9;looper++){
 			document.getElementsByClassName("arrow")[looper].style.animationPlayState = "paused";
 		}
+		var vid = document.getElementById("bgvid"); 
+		vid.pause();
+
 }
 function PlayArrow(){
 	if(playing==false){
+		var vid = document.getElementById("bgvid"); 
 		arrow.play();
+		vid.play();
 		playing=true;
 		for (looper=0;looper<9;looper++){
 			document.getElementsByClassName("arrow")[looper].style.animationPlayState = "running";
 		}
 	}
 	else if(playing==true){
+		var vid = document.getElementById("bgvid"); 
 		arrow.pause();
+		vid.pause();
 		playing=false;
 		for (looper=0;looper<9;looper++){
 			document.getElementsByClassName("arrow")[looper].style.animationPlayState = "paused";
 		}
+
 	}		
 }
 
